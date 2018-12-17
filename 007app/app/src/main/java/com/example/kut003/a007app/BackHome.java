215a4f03b9b58ebdaaad1e8fa87e6d2298b1c9fb
1200291 <katsu.tiger-0811@softbank.ne.jp>
2018-12-17 17:38:43 +0900

    ゲームの遷移がすべて終了しましたのでpushします

--- 007app/app/src/main/java/com/example/kut003/a007app/BackHome.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/BackHome.java
@@ -1,14 +1,22 @@
-//材料をお店で選択する画面(図14)
+//材料をお店で選択する画面(図15)
 package com.example.kut003.a007app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import java.util.Random;
+import android.widget.Toast;
+import android.view.Gravity;
 
 public class BackHome extends Activity {
+
+    private String lackedMessage = "LACKING";
+    private int completeCheck = 0;
+    Random rand = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +28,13 @@ public class BackHome extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Log.d("debug", "button1, Perform action on click");
-                Intent intent = new Intent(getApplication(), SubActivity2.class);
-                startActivity(intent);
+                completeCheck = rand.nextInt(2);
+                if(completeCheck == 0) {
+                    Intent intent = new Intent(getApplication(), CheckFood.class);
+                    startActivity(intent);
+                } else {
+                    toastMake(lackedMessage);
+                }
             }
         });
 
@@ -29,10 +42,18 @@ public class BackHome extends Activity {
         final Button button2 = findViewById(R.id.button_go);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+
                 Log.d("debug", "button2, Perform action on click");
                 Intent intent = new Intent(getApplication(), ChooseShop.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void toastMake(String message){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        // 位置調整
+        toast.setGravity(Gravity.CENTER, 0, -200);
+        toast.show();
     }
 }
