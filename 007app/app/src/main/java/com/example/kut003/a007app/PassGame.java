5544cae0ab0c36a0058c047f0cb6b6bbcc65f428
1200291 <katsu.tiger-0811@softbank.ne.jp>
2018-12-19 15:01:30 +0900

    ゲームのパスワード設定

--- 007app/app/src/main/java/com/example/kut003/a007app/PassGame.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/PassGame.java
@@ -2,18 +2,52 @@
 package com.example.kut003.a007app;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PassGame extends Activity {
+
+    public SharedPreferences dataPass;
+    public EditText editText;
+    private TextView textWrite, textView;
+    public String pass = "1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.pass_game);
+
+        //パスワード入力のためのインスタンスを生成
+        dataPass = getSharedPreferences("data", MODE_PRIVATE);
+
+        //入力できるようにする
+        editText = findViewById(R.id.edit_text);
+
+
+        //exitを押せるようにする
+        final Button button0 = findViewById(R.id.button_write);
+        button0.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String text = editText.getText().toString();  //入力内容を格納
+                if(pass.equals(text)){  //パスワードと入力を比較
+                    Log.d("debug", "button1, Perform action on click");
+                    Intent intent = new Intent(getApplication(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.d("debug", "button1, Perform action on click");
+                    Intent intent = new Intent(getApplication(), SubActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
         final Button button1 = findViewById(R.id.button_return);
         button1.setOnClickListener(new View.OnClickListener() {
