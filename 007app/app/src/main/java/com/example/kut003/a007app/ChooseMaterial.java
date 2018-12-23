d406b49deed96e9295a9550250fb2ef7b2cb4954
1200291 <katsu.tiger-0811@softbank.ne.jp>
2018-12-23 16:35:17 +0900

    Revert "Merge branch 'master' of https://github.com/007-kut/2018_SoftwareEngineering"
    This reverts commit 44a4578c501d90c5e65f7d0c24edfd90ad8704b8, reversing
    changes made to a269ec4ed03902a04ffca4ef5dca464fc0ba479f.

--- 007app/app/src/main/java/com/example/kut003/a007app/ChooseMaterial.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/ChooseMaterial.java
@@ -60,11 +60,7 @@ public class ChooseMaterial extends Activity {
         // 表示時間
         toast.setDuration(Toast.LENGTH_SHORT);
         // 位置調整
-<<<<<<< HEAD
         //toast.setGravity(Gravity.CENTER, 0, -100);
-=======
-        toast.setGravity(Gravity.CENTER, 0, 0);
->>>>>>> morimoto_server
         toast.show();
     }
 }
