165c92cf96c9a7ebf0e57d3e80e773ffd3034a7b
1200291 <katsu.tiger-0811@softbank.ne.jp>
2019-01-03 16:36:47 +0900

    質問入力ができる状態

--- 007app/app/src/main/java/com/example/kut003/a007app/MakePost.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/MakePost.java
@@ -7,14 +7,42 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MakePost extends Activity {
+
+    private EditText questionText;
+    private TextView textView;
+    private CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.make_post);
 
+        checkBox = findViewById(R.id.check);
+        checkBox.setChecked(false);
+        checkBox.setText("匿名");
+        // リスナーを登録
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            // タップされると呼び出される
+            @Override
+            public void onClick(View v) {
+                // チェックステータス取得
+                boolean check = checkBox.isChecked();
+                if(check){
+                    checkBox.setText("匿名");
+                }
+                else{
+                    checkBox.setText("ユーザ名");
+                }
+            }
+        });
+
+        questionText = findViewById(R.id.question_text);
         //子育て窓口画面に戻る
         final Button button0 = findViewById(R.id.button_return);
         button0.setOnClickListener(new View.OnClickListener() {
