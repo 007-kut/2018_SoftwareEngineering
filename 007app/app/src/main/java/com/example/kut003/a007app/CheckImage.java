f666dd61803051f2f642a7f5deea87648c8b8d1e
Satton <satoc1516@gmail.com>
2019-01-18 19:57:05 +0900

    [update] 成長記録ギャラリー位置の修正

--- 007app/app/src/main/java/com/example/kut003/a007app/CheckImage.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/CheckImage.java
@@ -9,14 +9,12 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.EditText;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
-import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -24,7 +22,6 @@ import java.io.FileDescriptor;
 import java.io.InputStream;
 import java.util.Locale;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.Point;
 import android.view.Display;
@@ -44,6 +41,7 @@ public class CheckImage  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_image);
 
+        //画像の表示
 
         // ウィンドウマネージャのインスタンス取得
         WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
@@ -56,17 +54,11 @@ public class CheckImage  extends AppCompatActivity {
         viewHeight = point.y;
 
         iv = (ImageView) findViewById(R.id.image_view);
-        //@Override
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        // Filter to show only images, using the image MIME data type.
         intent.setType("image/*");
         startActivityForResult(intent, READ_REQUEST_CODE);
 
-        //画像の表示
-        //画像URIの受け渡し
-        //Intent intent = getIntent();
-        //Uri uri = intent.getIntExtra(GrowHome.EXTRA_DATA);
 
         //もどるボタン
         final Button button_back = findViewById(R.id.button_viewer_back);
@@ -120,36 +112,21 @@ public class CheckImage  extends AppCompatActivity {
             }
         });*/
     }
-    //トースト表示
-    /*
-    private void toastMake() {
-        String message = "正しく入力してください";
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-        // 位置調整
-        toast.setGravity(Gravity.CENTER, 0, -200);
-        toast.show();
-    }*/
     Intent resultData;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultData){
-        //super.onActivityResult(requestCode, resultCode, resultData);
-        //iv = (ImageView)findViewById(R.id.imageView);
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Uri uri = null;
             if (resultData != null) {
                 uri = resultData.getData();
                 try {
                     // Uriを表示
-                    // textView.setText(String.format(Locale.US, "Uri:　%s", uri.toString()));
-                    //Bitmap bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                     bmp = loadImage(uri, viewWidth, viewHeight);
                     canvas = new Canvas(bmp);
                     iv.setImageBitmap(bmp);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                //canvas = new Canvas(bmp);
-                //iv.setImageBitmap(bmp);
             }
         }
     }
