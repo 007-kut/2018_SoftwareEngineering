af6a48578fe8750b8bea23bd4800f0f3ea5c745b
Yuuki-Onoda <sakusya_kokoriko@icloud.com>
2019-01-22 15:27:13 +0900

    お絵かき完成。保存可能に。

--- 007app/app/src/main/java/com/example/kut003/a007app/MakeArt.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/MakeArt.java
@@ -12,16 +12,77 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.Locale;
+
 public class MakeArt extends AppCompatActivity implements View.OnClickListener{
 
     private PaintView drawingView;
     private String savedMessage;    //保存確認画面を表示するために使う
+
+    private static final int REQUEST_SAVE_IMAGE = 1002;
+    private TextView textView;
+
+    private MediaProjectionManager mpManager;
+    private MediaProjection mProjection;
+    private static final int REQUEST_MEDIA_PROJECTION = 1001;
+
+    private int displayWidth, displayHeight;
+    private ImageReader imageReader;
+    private VirtualDisplay virtualDisplay;
+    private int screenDensity;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.make_art);
+
+        textView = findViewById(R.id.text_view);
+        imageView = findViewById(R.id.image_view);
+
+        // 画面の縦横サイズとdpを取得
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenDensity = displayMetrics.densityDpi;
+        displayWidth = displayMetrics.widthPixels;
+        displayHeight = displayMetrics.heightPixels;
+
+        mpManager = (MediaProjectionManager)
+                getSystemService(MEDIA_PROJECTION_SERVICE);
+
+        // permissionを確認するintentを投げ、ユーザーの許可・不許可を受け取る
+        if(mpManager != null){
+            startActivityForResult(mpManager.createScreenCaptureIntent(),
+                    REQUEST_MEDIA_PROJECTION);
+        }
 
         //ゲーム選択画面にもどる
         final Button button0 = findViewById(R.id.button_albam_back);
@@ -39,6 +100,13 @@ public class MakeArt extends AppCompatActivity implements View.OnClickListener{
         final Button button1 = findViewById(R.id.button_save);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                getScreenshot();
+
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_TITLE, "img.png");
+
+                startActivityForResult(intent, REQUEST_SAVE_IMAGE);
                 toastMake(savedMessage);
             }
         });
@@ -114,5 +182,82 @@ public class MakeArt extends AppCompatActivity implements View.OnClickListener{
         // 位置調整
         toast.setGravity(Gravity.CENTER, 0, -200);
         toast.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (REQUEST_MEDIA_PROJECTION == requestCode) {
+            if (resultCode != RESULT_OK) {
+                // 拒否された
+                Toast.makeText(this,
+                        "User cancelled", Toast.LENGTH_LONG).show();
+                return;
+            }
+            // 許可された結果を受け取る
+            setUpMediaProjection(resultCode, resultData);
+        }
+        if (requestCode == REQUEST_SAVE_IMAGE && resultCode == Activity.RESULT_OK) {
+            if(resultData.getData() != null){
+
+                Uri uri = resultData.getData();
+
+                // Uriを表示
+                textView.setText(
+                        String.format(Locale.US, "Uri:　%s",uri.toString()));
+
+                try(OutputStream outputStream =
+                            getContentResolver().openOutputStream(uri);) {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void setUpMediaProjection(int code, Intent intent) {
+        mProjection = mpManager.getMediaProjection(code, intent);
+        setUpVirtualDisplay();
+    }
+
+    private void setUpVirtualDisplay() {
+        imageReader = ImageReader.newInstance(
+                displayWidth, displayHeight, PixelFormat.RGBA_8888, 2);
+
+        virtualDisplay = mProjection.createVirtualDisplay("ScreenCapture",
+                displayWidth, displayHeight, screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                imageReader.getSurface(), null, null);
+    }
+
+    private void getScreenshot() {
+        // ImageReaderから画面を取り出す
+        Log.d("debug", "getScreenshot");
+
+        Image image = imageReader.acquireLatestImage();
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer buffer = planes[0].getBuffer();
+
+        int pixelStride = planes[0].getPixelStride();
+        int rowStride = planes[0].getRowStride();
+        int rowPadding = rowStride - pixelStride * displayWidth;
+
+        // バッファからBitmapを生成
+        bitmap = Bitmap.createBitmap(
+                displayWidth + rowPadding / pixelStride, displayHeight,
+                Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(buffer);
+        image.close();
+
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (virtualDisplay != null) {
+            Log.d("debug","release VirtualDisplay");
+            virtualDisplay.release();
+        }
+        super.onDestroy();
     }
 }
