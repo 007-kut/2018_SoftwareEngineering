1929cd1f4b0bdcf2d7e9a532524e3bba3a4ea50b
1200291 <katsu.tiger-0811@softbank.ne.jp>
2019-01-22 13:31:58 +0900

    無視

---
+++ 007app/app/src/main/java/com/example/kut003/a007app/InBasket.java
@@ -0,0 +1,56 @@
+//持っている材料を確認(図13)
+package com.example.kut003.a007app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class InBasket extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.in_basket);
+        final ShareQuestion sq = (ShareQuestion) this.getApplication();
+
+        final TextView textView1 = findViewById(R.id.textView1);
+        int countBeef = sq.getBeef();
+        textView1.setText(String.valueOf(countBeef));
+
+        final TextView textView2 = findViewById(R.id.textView2);
+        int countPork = sq.getPork();
+        textView2.setText(String.valueOf(countPork));
+
+        final TextView textView3 = findViewById(R.id.textView3);
+        int countCarrots = sq.getCarrots();
+        textView3.setText(String.valueOf(countCarrots));
+
+        final TextView textView4 = findViewById(R.id.textView4);
+        int countOnion = sq.getOnion();
+        textView4.setText(String.valueOf(countOnion));
+
+        final TextView textView5 = findViewById(R.id.textView5);
+        int countEgg = sq.getEgg();
+        textView5.setText(String.valueOf(countEgg));
+
+        final TextView textView6 = findViewById(R.id.textView6);
+        int countMilk = sq.getMilk();
+        textView6.setText(String.valueOf(countMilk));
+
+
+
+        final Button button1 = findViewById(R.id.button_back);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.d("debug", "button1, Perform action on click");
+                Intent intent = new Intent(getApplication(), ChooseShop.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ 007app/app/src/main/java/com/example/kut003/a007app/ShareQuestion.java
@@ -0,0 +1,111 @@
+//QIDと質問をクラス間で共有するためのファイル
+package com.example.kut003.a007app;
+
+import android.app.Application;
+
+public class ShareQuestion extends Application {
+    //子育て窓口で使う
+    private String [] savedContents = new String [10];
+    private String [] SavedId = new String [10];
+    private String chooseId;
+    private String chooseName;
+    private String chooseArea;
+    private String chooseContents;
+    private String chooseAnonimity;
+
+    public  String[] getContents () {
+        return this.savedContents;
+    }
+    public String[] getId() {
+        return this.SavedId;
+    }
+    public String getChooseId() {
+        return this.chooseId;
+    }
+    public String getChooseName() {
+        return this.chooseName;
+    }
+    public String getChooseArea() {
+        return this.chooseArea;
+    }
+    public String getChooseContents () {
+        return this.chooseContents;
+    }
+    public String getChooseAnonimity() {
+        return this.chooseAnonimity;
+    }
+    public void setContents(String[] array) {
+        this.savedContents = array;
+    }
+    public void setId(String[] array) {
+        this.SavedId = array;
+    }
+    public void setChooseQuestion(String id, String name, String area, String contents, String anonimity) {
+        this.chooseId = id;
+        this.chooseName = name;
+        this.chooseArea = area;
+        this.chooseContents = contents;
+        this.chooseAnonimity = anonimity;
+    }
+
+    //おつかいで使う
+    private String chooseFood = "";
+    private String chooseShop = "";
+    private int onion = 0;
+    private int carrots = 0;
+    private int pork = 0;
+    private int beef = 0;
+    private int egg = 0;
+    private int milk = 0;
+
+    public void setChooseFood(String food) {
+        this.chooseFood = food;
+    }
+    public void setChooseShop(String shop) {
+        this.chooseShop = shop;
+    }
+    public void setCountOnion(int count) {
+        this.onion = count;
+    }
+    public void setCountPork(int count) {
+        this.pork = count;
+    }
+    public void setCountCarrots(int count) {
+        this.carrots = count;
+    }
+    public void setCountBeef(int count) {
+        this.beef = count;
+    }
+    public void setCountEgg(int count) {
+        this.egg = count;
+    }
+    public void setCountMilk(int count) {
+        this.milk = count;
+    }
+    public String getChooseFood() {
+        return this.chooseFood;
+    }
+    public String getChooseShop() {
+        return this.chooseShop;
+    }
+    public int getOnion() {
+        return this.onion;
+    }
+    public int getPork() {
+        return this.pork;
+    }
+    public int getCarrots() {
+        return this.carrots;
+    }
+    public int getBeef() {
+        return this.beef;
+    }
+    public int getEgg() {
+        return this.egg;
+    }
+    public int getMilk() {
+        return this.milk;
+    }
+}
+
+
