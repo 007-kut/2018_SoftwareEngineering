2387d0ed030cd3d7cf66c6f5447e43e70f985081
1200381 <32477160+1200381@users.noreply.github.com>
2019-01-24 02:03:35 +0900

    png画像の圧縮。まだエラー出ます

--- 007app/app/src/main/java/com/example/kut003/a007app/ChooseMaterial.java
+++ 007app/app/src/main/java/com/example/kut003/a007app/ChooseMaterial.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ImageButton;
 import android.widget.TextView;
